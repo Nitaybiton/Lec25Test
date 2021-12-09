6957980c9263c0209dfc936f2c0ea7bfb972971a
Nitay Biton <nitaybiton@gmail.com>
2021-12-09 18:14:22 +0200

    added cocoapods

---
+++ Pods/Target Support Files/Pods-Lec25Nitay-Lec25NitayUITests/Pods-Lec25Nitay-Lec25NitayUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lec25Nitay_Lec25NitayUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Lec25Nitay_Lec25NitayUITests
+@end
---
+++ Pods/Target Support Files/Pods-Lec25NitayTests/Pods-Lec25NitayTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Lec25NitayTests : NSObject
+@end
+@implementation PodsDummy_Pods_Lec25NitayTests
+@end
